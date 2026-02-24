e31cd24747b7b00611fe9dc3913f01dffe30dbf2
klanpakwaenchananyoo <klanpakwaenchananyoo@gmail.com>
2026-02-24 15:27:23 +0700

    average.cpp

--- average.cpp
+++ average.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip>
 
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    if (argc <= 1) {
+        cout << "Please input numbers to find average." << endl;
+        return 0;
+    }
+
+    double sum = 0;
+    int count = argc - 1;
+
+    for (int i = 1; i < argc; i++) {
+        try {
+           
+            sum += stod(argv[i]);
+        } catch (...) {
+           
+            return 1; 
+        }
+    }
+
+    double average = sum / count;
+
+   
+    cout << "---------------------------------" << endl;
+    cout << "Average of " << count << " numbers = " << average << endl;
+    cout << "---------------------------------" << endl;
+
+    return 0;
+}
